c61e7eef1cc4f2b151d6d2ffd84dfccdf10b27a7
Lins-20 <lins20@proton.me>
2024-02-06 14:35:23 +0800

    deploy for lins20facrtoryV2

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,8 +1,9 @@
 import { ethers, upgrades } from "hardhat";
 
 async function main() {
-    const factory: any = await ethers.getContractFactory("Lins20Factory");
-    const proxy = await upgrades.deployProxy(factory, [], {
+    const impl = "0x97700d961b8f14b27973df18758e92ee591e373a"; // the lins20v2 address, if not deployed, set it to ethers.ZeroAddress
+    const factory: any = await ethers.getContractFactory("Lins20FactoryV2");
+    const proxy = await upgrades.deployProxy(factory, [impl], {
         initializer: "initialize",
         kind: "uups",
     });
