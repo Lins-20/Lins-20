27606b5d9b51a9597311e7e339d674af599ad43a
Lins-20 <lins20@proton.me>
2024-02-06 16:30:49 +0800

    add recover scripts

---
+++ scripts/recover.ts
@@ -0,0 +1,53 @@
+// migrate LINS script
+ 
+import fs from "fs";
+import { parse } from "csv-parse";
+import { ethers } from "hardhat";
+
+const loadCSV = async () => {
+    const records = [];
+    const parser = fs
+        .createReadStream(`${__dirname}/../resources/holders.csv`)
+        .pipe(parse({
+        }));
+    for await (const record of parser) {
+        records.push(record);
+    }
+    return records;
+};
+
+const address = "0xbe5589c967CCEC8dE72236e140A56bbabD794FDc";  // new lins20 address
+const size = 1000;
+
+async function action(data: string[]) {
+    const contract = await ethers.getContractAt("Lins20V2", address);
+    const res: any = await contract.recover(data);
+    const receipt = await res.wait();
+    return receipt.status === 1;
+}
+
+(async () => {
+    const records = await loadCSV();
+    const address = records.slice(1).map(s => s[0])
+    console.log(address.slice(0, 100));
+    return;
+
+
+    let page = 0;
+    while (1) {
+        let start = page * size;
+        let end = start + size > address.length ? address.length : start + size;
+        const data = address.slice(page * size, (page + 1) * size);
+        if (await action(data)) {
+            page++;
+            console.log("recover ok", page);
+        } else {
+            console.log("recover fail", page)
+            break;
+        }
+        if (end == address.length) {
+            console.log("task over");
+            break;
+        }
+    }
+})();
