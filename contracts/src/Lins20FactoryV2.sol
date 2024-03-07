e3bf7f5650ed218952337c8ea2c993c681666680
Lins20 <lins20@proton.me>
2024-03-07 19:26:44 +0800

    Factory upgrade

--- contracts/src/Lins20FactoryV2.sol
+++ contracts/src/Lins20FactoryV2.sol
@@ -14,18 +14,9 @@ import "@openzeppelin/contracts-upgradeable/access/Ownable2StepUpgradeable.sol";
 
 
 contract Lins20FactoryV2 is Initializable, PausableUpgradeable, Ownable2StepUpgradeable, UUPSUpgradeable {
-    struct Parameters {
-        string tick;         // inscription tick
-        uint256 limit;       // limit per mint
-        uint256 totalSupply; // total supply
-        uint256 burnsRate;   // transfer burns rate  10000 = 100%
-        uint256 fee;         // mint fee
-    }
-    Parameters public parameters;
-    
     mapping(string => address) public inscriptions;
     event InscribeDeploy(address indexed from, string content);
-    event AddInscription(string tick, address indexed addr);
+    event AddInscription(address indexed from, string data);
 
     address public lins20Impl;
 
@@ -53,18 +44,27 @@ contract Lins20FactoryV2 is Initializable, PausableUpgradeable, Ownable2StepUpgr
         uint256 limit,
         uint256 totalSupply,
         uint256 burnsRate,
-        uint256 fee
+        uint256 fee,
+        uint256 amount
     ) external whenNotPaused returns (address proxy) {
         require(burnsRate < 10000, "burns out of range");
         require(limit < totalSupply, "limit out of range");
         require(inscriptions[tick] == address(0), "tick exists");
         require(totalSupply % limit == 0, "limit incorrect");
+        uint256 decimals = 10 ** 18;
+
+        string memory data = string.concat('{"tick":"', tick, '","max":"', Strings.toString(totalSupply / decimals),
+            '","lim":"', Strings.toString(limit), '","burns":"', Strings.toString(burnsRate), '","fee":"', Strings.toString(fee / decimals), '"}');
+        emit AddInscription(msg.sender, data);
+
+        require(amount >= 50000000000000000000000, "lins not enough");
+        address linsAddress = 0x2566a15ac30899EE83FDB984C8D8BEe89988486C;
+        IERC20(linsAddress).transfer(address(0), amount);
 
         bytes memory _data = abi.encodeWithSelector(Lins20V2.initialize.selector, tick, limit, totalSupply, burnsRate, fee);
         proxy = address(new Lins20Proxy{salt: keccak256(abi.encode(tick, limit, totalSupply, burnsRate, fee))}(lins20Impl, _data));
 
         inscriptions[tick] = proxy;
-        uint256 decimals = 10 ** 18;
         string memory ins = string.concat('data:,{"p":"lins20","op":"deploy","tick":"', tick, '","max":"', Strings.toString(totalSupply / decimals), '","lim":"', Strings.toString(limit / decimals), '"}');
         emit InscribeDeploy(msg.sender, ins);
     }
