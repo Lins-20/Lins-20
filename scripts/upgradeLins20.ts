5344dc6374ca319d9f64dea6af68c3fec39b4a6b
Lins-20 <lins20@proton.me>
2024-03-08 11:07:51 +0800

    update scripts

--- scripts/upgradeLins20.ts
+++ scripts/upgradeLins20.ts
@@ -1,20 +1,20 @@
 import {ethers} from "hardhat";
 
 async function main() {
-    // const factoryAddr = "0xA63BE2b5A5ce267fFf404a84C9b567023CbC8ab0"; 
-    const lins20_Addr = "0xbe5589c967CCEC8dE72236e140A56bbabD794FDc"; // old impl
-    // const res = await ethers.deployContract("Lins20V2");
-    const implNew = "0x4aC2DE62bcdC15944C8c7C6aD1243E591BAC00f0"
-
-    const lins20 = await ethers.getContractAt("Lins20Proxy", lins20_Addr);
+    const factoryAddr = "0x5cb5Dc71E652CAeF6791e9750d55aEC46aC38835"; 
+    const lins20proxy = "0x2566a15ac30899EE83FDB984C8D8BEe89988486C"; // 
+    const res = await ethers.deployContract("Lins20V2");
+    const implNew = await res.getAddress();
+    console.log("deploy lins20 impl", implNew);
+    
+    const lins20 = await ethers.getContractAt("Lins20Proxy", lins20proxy);
     const upgrade: any = await lins20.upgradeTo(implNew);
     await upgrade.wait();
     console.log("upgrade lins20 success", upgrade.status);
 
-    // const factory = await ethers.getContractAt("Lins20FactoryV2", factoryAddr);
-    // await factory.setLins20Impl(implNew);
-
-    // console.log("set factory impl success");
+    const factory = await ethers.getContractAt("Lins20FactoryV2", factoryAddr);
+    await factory.setLins20Impl(implNew);
+    console.log("set factory impl success");
 }
 
 main();
